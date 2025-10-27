1dc295f02b0be46d901b112fa0e7c7b6ba505e94
nisrina salsa <nisrinahikmawan@gmail.com>
2025-10-27 09:28:38 +0700

    menambahkan index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-'use strict'
+const express = require('express')
+const app = express();
+const PORT= 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.listen(PORT, () => {
+    console.log('Server Started on PORT 3000');
+})
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server Started');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
